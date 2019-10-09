78a49ae0593138f33caca95c7ff6e673b89a4d1f
Eric <liyangzhong@hotmail.com>
2019-10-10 07:09:38 +0800

    update

--- index.js
+++ index.js
@@ -437,3 +437,29 @@
 // console.log(circle);
 //============
 
+//every object has a constructor property
+
+//Factory Function
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log('draw');
+    }
+  };
+}
+const circle = createCircle(1);
+
+console.log(circle.constructor);
+
+//Constructor Function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log('draw');
+  }
+}
+
+const another = new Circle(1);
+
+console.log(another.constructor);
